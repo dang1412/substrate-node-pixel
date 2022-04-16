48220f1661dab0f98bf3bea192b2eac1f8907021
dang1412 <dttung1412@gmail.com>
2022-04-16 14:23:34 +0700

    pixel: mint, set_price, set_image

--- pallets/pixel/src/lib.rs
+++ pallets/pixel/src/lib.rs
@@ -83,8 +83,12 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		/// Mint pixel. [account, pixel_id]
 		Minted(T::AccountId, u32),
+		/// For sale. [account, pixel_id, price]
+		PriceSet(T::AccountId, u32, Option<BalanceOf<T>>),
 		/// Buy pixel. [seller, buyer, pixel_id, price]
 		Buy(T::AccountId, T::AccountId, u32, BalanceOf<T>),
+		/// Image set [account, pixel_id, image_url, size]
+		ImageSet(T::AccountId, u32, Option<Vec<u8>>, (u32, u32))
 	}
 
 	// Errors inform users that something went wrong.
@@ -94,6 +98,10 @@ pub mod pallet {
 		// NoneValue,
 		// /// Errors should have helpful documentation associated with them.
 		// StorageOverflow,
+		PixelAlreadyMinted,
+		ExceedMaxPixelOwned,
+		PixelNotExist,
+		NotPixelOwner,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -104,9 +112,17 @@ pub mod pallet {
 
 		#[pallet::weight(100)]
 		#[transactional]
-		pub fn mint_pixel(origin: OriginFor<T>, id: u32) -> DispatchResult {
+		pub fn mint_pixel(origin: OriginFor<T>, pixel_id: u32) -> DispatchResult {
 			// check maximum owned pixels
 			// mint
+			let sender = ensure_signed(origin)?;
+
+			Self::mint(&sender, pixel_id)?;
+
+			// Logging to the console
+			// log::info!("A pixel is minted with ID: {:?}.", pixel_id);
+			// Deposit our "Created" event.
+			Self::deposit_event(Event::Minted(sender, pixel_id));
 			Ok(())
 		}
 
@@ -114,17 +130,50 @@ pub mod pallet {
 		#[transactional]
 		pub fn batch_mint_pixels(origin: OriginFor<T>, pixel_ids: BoundedVec<u32, T::MaxPixelBatchMint>) -> DispatchResult {
 			// check maximum owned pixels
+			let sender = ensure_signed(origin)?;
+
 			// batch mint
+			for pixel_id in pixel_ids.iter() {
+				Self::mint(&sender, *pixel_id)?;
+			}
 			Ok(())
 		}
 
 		#[pallet::weight(100)]
 		pub fn set_price(origin: OriginFor<T>, pixel_id: u32, new_price: Option<BalanceOf<T>>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			// Ensure the pixel exists and is called by the pixel owner
+			ensure!(Self::is_pixel_owner(&pixel_id, &sender)?, <Error<T>>::NotPixelOwner);
+
+			let mut pixel = Self::pixels(&pixel_id).ok_or(<Error<T>>::PixelNotExist)?;
+
+			pixel.price = new_price;
+			<Pixels<T>>::insert(&pixel_id, pixel);
+
+			// Deposit a "PriceSet" event.
+			Self::deposit_event(Event::PriceSet(sender, pixel_id, new_price));
+
 			Ok(())
 		}
 
 		#[pallet::weight(100)]
 		pub fn set_image(origin: OriginFor<T>, pixel_id: u32, new_image: Option<Vec<u8>>, size: (u32, u32)) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			// Ensure the pixel exists and is called by the pixel owner
+			ensure!(Self::is_pixel_owner(&pixel_id, &sender)?, <Error<T>>::NotPixelOwner);
+
+			// TODO check owner for all covered pixels
+
+			let mut pixel = Self::pixels(&pixel_id).ok_or(<Error<T>>::PixelNotExist)?;
+
+			pixel.image = new_image.clone();
+			<Pixels<T>>::insert(&pixel_id, pixel);
+
+			// Deposit a "PriceSet" event.
+			Self::deposit_event(Event::ImageSet(sender, pixel_id, new_image, size));
+
 			Ok(())
 		}
 
@@ -146,8 +195,30 @@ pub mod pallet {
 			owner: &T::AccountId,
 			pixel_id: u32,
 		) -> Result<(), Error<T>> {
-			// check pixel exists
+			// TODO check pixel exists
+
+			let pixel = Pixel::<T> {
+				pixel_id,
+				image: None,
+				price: None,
+				owner: owner.clone(),
+                date_minted: T::Time::now(),
+			};
+
+			<PixelsOwned<T>>::try_mutate(&owner, |pixel_vec| {
+				pixel_vec.try_push(pixel_id)
+			}).map_err(|_| <Error<T>>::ExceedMaxPixelOwned)?;
+
+			<Pixels<T>>::insert(pixel_id, pixel);
+
 			Ok(())
+		}
+
+		pub fn is_pixel_owner(pixel_id: &u32, acct: &T::AccountId) -> Result<bool, Error<T>> {
+			match Self::pixels(pixel_id) {
+				Some(pixel) => Ok(pixel.owner == *acct),
+				None => Err(<Error<T>>::PixelNotExist)
+			}
 		}
 	}
 }
